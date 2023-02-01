0d8064d1c0ac1eaadc7bf795fa246aaf7201a486
Nilesh <nileshparab5623@gmail.com>
2023-02-01 22:57:17 +0530

    BOP 1

---
+++ 04 Basics of Programming -I/03_Sizeof_Operator.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    char a ='a';
+    short b = 10;
+    int c = 10;
+    long d = 10;
+    long long e = 10;
+    float f = 10;
+    double g = 10;
+
+    cout<<"Size of char:"<<sizeof(a)<<"\n";
+    cout<<"Size of short:"<<sizeof(b)<<"\n";
+    cout<<"Size of int:"<<sizeof(c)<<"\n";
+    cout<<"Size of long:"<<sizeof(d)<<"\n";
+    cout<<"Size of long long:"<<sizeof(e)<<"\n";
+    cout<<"Size of float:"<<sizeof(f)<<"\n";
+    cout<<"Size of double:"<<sizeof(g)<<"\n";  
+    
+}
---
+++ 04 Basics of Programming -I/04_Typecasting.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    // Implicit Typecasting
+    char a = 65;
+    cout<<a<<"\n";
+
+    //Explicit Typecasting
+    float b = 2.12;
+    int c = (int)b;
+    cout<<c; 
+    
+}
