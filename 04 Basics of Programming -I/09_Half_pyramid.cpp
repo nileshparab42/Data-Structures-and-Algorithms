8e93f98c9695345e070a1e5f3ff1723e701c809b
Nilesh <nileshparab5623@gmail.com>
2023-03-07 23:37:18 +0530

    loops

--- 04 Basics of Programming -I/09_Half_pyramid.cpp
+++ 04 Basics of Programming -I/09_Half_pyramid.cpp
@@ -4,7 +4,7 @@ int main(){
     int count;
     cin>>count;
     for(int i=count; i>0; i--){
-        //We can also use count-j if first loop is incremental loops 3
+        //We can also use count-j if first loop is incremental loops 4
         for(int j=1; j<=i; j++){
             cout<<"* ";
         }
