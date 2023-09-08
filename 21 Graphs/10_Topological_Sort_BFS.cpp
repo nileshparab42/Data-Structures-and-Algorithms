bd27f356b718ec09d8ca282c00393288cf3bd367
Nilesh <nileshparab5623@gmail.com>
2023-09-08 23:53:09 +0530

    Last github push

---
+++ 21 Graphs/10_Topological_Sort_BFS.cpp
@@ -0,0 +1,219 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+#include<list>
+#include<queue>
+#include<stack>
+using namespace std;
+
+class graph{
+    public:
+    unordered_map<int,list<int>> adj;
+
+    void addNode(int u,int v,int type){
+        // type 0 -> Undirected
+        // type 1 -> Directed
+        adj[u].push_back(v);
+        if(type==0){
+            adj[v].push_back(u);
+        }
+    } 
+
+    void printGraph(){
+        for(auto node:adj){
+            cout<<node.first<<": ";
+            for(auto neb:node.second){
+                cout<<"("<<neb<<")";
+            }
+            cout<<endl;
+
+        }
+    }
+
+
+
+    void BFS(int src,unordered_map<int,bool>& visited){
+    queue<int> q;
+    q.push(src);
+    visited[src]=true;
+    while(!q.empty()){
+        int node=q.front();
+        cout<<node<<" ";
+        q.pop();
+        for(auto i:adj[node]){
+                if(visited[i]!=true){
+                    q.push(i);
+                    visited[i]=true;
+                }
+            }
+        }
+    }
+
+    bool cycleDetBFS(int src,unordered_map<int,bool>& visited){
+    unordered_map<int,int> par;
+    queue<int> q;
+    q.push(src);
+    par[src]=-1;
+    visited[src]=true;
+    while(!q.empty()){
+        int node=q.front();
+        q.pop();
+        for(auto neb:adj[node]){
+                if(visited[neb]!=true){
+                    q.push(neb);
+                    visited[neb]=true;
+                    par[neb]=node;
+                }
+                else{
+                    if(par[node]!=neb){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    void DFS(int src,unordered_map<int,bool>& visited2){
+        visited2[src]=true;
+        cout<<src<<" ";
+        for(auto i:adj[src]){
+            if(visited2[i]!=true){
+                DFS(i,visited2);
+            }
+        }
+    }
+
+    bool UndirectedCycleDetDFS(int src,unordered_map<int,bool>& visited2,int parent){
+        visited2[src]=true;
+        for(auto neb:adj[src]){
+            if(visited2[neb]!=true){
+                bool ans = UndirectedCycleDetDFS(neb,visited2,src);
+                if(ans==true){
+                    return true;
+                }
+            }
+            else{
+                if(neb != parent){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    bool DirectedCycleDetDFS(int src,unordered_map<int,bool>& visited2,unordered_map<int,bool>& dfsvisited){
+        visited2[src]=true;
+        dfsvisited[src]=true;
+        for(auto neb:adj[src]){
+            if(visited2[neb]!=true){
+                bool ans = DirectedCycleDetDFS(neb,visited2,dfsvisited);
+                if(ans==true){
+                    return true;
+                }
+            }
+            else{
+                if(dfsvisited[neb]==true){
+                    return true;
+                }
+            }
+        }
+        dfsvisited[src]=false;
+        return false;
+    }
+
+    void topoDFS(int src,unordered_map<int,bool>& visited,stack<int>& st){
+        visited[src]=true;
+        cout<<src<<" ";
+        for(auto i:adj[src]){
+            if(visited[i]!=true){
+                topoDFS(i,visited,st);
+            }
+        }
+        st.push(src);
+    }
+
+    void topoBFS(int n,vector<int>& ans){
+        unordered_map<int,int> indegree;
+        for(auto node:adj){
+            for(auto neb:adj[node.first]){
+                indegree[neb]++;
+            }
+        }
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+
+        // for(auto i:indegree){
+        //     cout<<i.first<<" "<<i.second<<"\n";
+        //     if(i.second==0){
+        //         q.push(i.first);
+        //     }
+        // }
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+
+            for(auto neb:adj[node]){
+                indegree[neb]--;
+                if(indegree[neb]==0){
+                    q.push(neb);
+                }
+            }
+        }
+
+    }    
+};
+
+
+
+
+
+int main(){
+    graph g;
+    g.addNode(0,1,1);
+    g.addNode(1,2,1);
+    g.addNode(2,3,1);
+    g.addNode(3,4,1);
+    g.addNode(3,5,1);
+    g.addNode(5,6,1);
+    g.addNode(4,6,1);
+    g.addNode(6,7,1);
+    unordered_map<int,bool> visited;
+    unordered_map<int,bool> dfsvisited;
+    stack<int> st;
+    // bool ans=false;
+    // for(int i=0;i<7;i++){
+    //     if(visited[i]!=true){
+    //         ans = g.DirectedCycleDetDFS(i,visited,dfsvisited);
+    //         if(ans==true){
+    //             break;
+    //         }
+    //     }
+    // }
+    vector<int> ans;
+    int n=8;
+    g.topoBFS(n,ans);
+    cout<<"Topological Sort(Kahn's Algorithm): ";
+    for(auto i:ans){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    //Kans Algorithm
+    if(ans.size()==n){
+        cout<<"Cycle is not Present";
+    }
+    else{
+        cout<<"Cycle is Present";
+    }
+    // g.printGraph();
+
+   
+
+    return 0;
+}
---
+++ 21 Graphs/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+for(auto i:indegree)
+        //     if(i.second==0){
+        //         q.push(i.first);
+        //     }
+        // }
